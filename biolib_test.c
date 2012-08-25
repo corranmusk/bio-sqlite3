ce50de9cd3deacb33c77e4560bce8f2a8dd8c89d
Corran Musk <corran.musk@googlemail.com>
2012-08-25 02:40:02 +0100

    End of the night.

--- biolib_test.c
+++ biolib_test.c
@@ -9,13 +9,17 @@ Corran C. Musk 2012
 #include "biolib.h"
 
 int main() {
-	unsigned char *z;
+	unsigned char *seq1, *seq2;
 	int int_result;
 	double dbl_result;
 
-	z="AAAAATCATGCATGCTA";
+	seq1="AAAAATCATGCATGCTA";
+	seq2="CTAGCTAGGCATGATCC";
 
-	dbl_result=libcgContentFunc(z);
+	dbl_result=libcgContentFunc(seq1);
 	printf("C+G Content : %f\n",dbl_result);
+	int_result=libLevenshteinDistFunc(seq1,seq2);
+	printf("Levenshtein distance : %d\n",int_result);
+
 	return 0;
 }
